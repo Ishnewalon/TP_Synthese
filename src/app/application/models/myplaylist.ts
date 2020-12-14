1c5b5dddfd2f01003fed5ec83914d94aabaccb81
Stephanie <ishnewalon@hotmail.com>
2020-12-14 15:54:48 -0500

    Added to playlist, made things look nice and added Contact page

--- src/app/application/models/myplaylist.ts
+++ src/app/application/models/myplaylist.ts
@@ -1,27 +1,27 @@
 export const PLAYLIST: any[]=
 [
     {
-        year: '2020',
+        year: 'Christmas Music',
+        items: [
+            {url: 'j9R9lztJaoA', range: '1', author: 'Mavis Staples', title: 'Christmas Vacation'},
+            {url: '94Ye-3C1FC8', range: '2', author: 'Paul McCartney', title: 'Wonderful Christmastime'},
+            {url: 'bjQzJAKxTrE', range: '3', author: 'Band Aid', title: 'Do They Know It\' Christmastime?'}
+        ]
+    },
+    {
+        year: 'Random Music',
         items: [
             {url: 'Ut-fJCc0zS4', range: '1', author: 'KekeFlipNote', title: 'Pigeon Conga'},
-            {url: '', range: '2', author: '', title: ''},
-            {url: '', range: '3', author: '', title: ''}
+            {url: 'D4EzD29ugjU', range: '2', author: 'Cristina D\'Avena', title: 'Sailor Moon Dance Remix'},
+            {url: 'v4xZUr0BEfE', range: '3', author: 'The HU', title: 'Yuve Yuve Yu'}
         ]
     },
     {
-        year: '2019',
+        year: 'Relaxing Music',
         items: [
-            {url: '', range: '1', author: '', title: ''},
-            {url: '', range: '2', author: '', title: ''},
-            {url: '', range: '3', author: '', title: ''}
-        ]
-    },
-    {
-        year: '2018',
-        items: [
-            {url: '', range: '1', author: '', title: ''},
-            {url: '', range: '2', author: '', title: ''},
-            {url: '', range: '3', author: '', title: ''}
+            {url: 'pKM8ieYWsjM', range: '1', author: 'Ben Briggs', title: 'Rainy Day'},
+            {url: 'xkR_HwZt2T8', range: '2', author: 'GameChops', title: 'Route 1'},
+            {url: 'OWYcUWyqzdI', range: '3', author: 'Mikel', title: 'Kakariko Village'}
         ]
     },
 ];
